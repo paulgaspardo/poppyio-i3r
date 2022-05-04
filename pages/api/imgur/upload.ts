1fc3cc9834d306a8026a4a972e86d8a2cd6537cc
Paul Gaspardo <paulgaspardo@outlook.com>
2022-05-04 10:31:25 +0200

    indent 2 spaces for consistency with the rest

--- pages/api/imgur/upload.ts
+++ pages/api/imgur/upload.ts
@@ -4,117 +4,117 @@ import getRawBody from "raw-body";
 const clientId = process.env.IMGUR_CLIENT_ID;
 
 export const config = {
-   api: {
-      bodyParser: false
-   }
+	api: {
+		bodyParser: false
+	}
 };
 
 let uploadsAllowed = 0;
 let nextCreditCheckAt = 0;
 function updateRateLimitInfo(responseHeadersOrCreditsApiData: {get(key:string):string|null|undefined}) {
-   let get = (key: string) => responseHeadersOrCreditsApiData.get(key) || responseHeadersOrCreditsApiData.get('x-ratelimit-' + key.toLowerCase());
-   uploadsAllowed = Math.floor(parseInt(get('ClientRemaining') || '0') / 10);
-   uploadsAllowed = Math.min(uploadsAllowed, Math.floor(parseInt(get('UserRemaining') || '0') / 10));
-   let postsRemaining = get('x-post-rate-limit-remaining');
-   if (typeof postsRemaining === 'string') {
-      uploadsAllowed = Math.min(uploadsAllowed, parseInt(postsRemaining));
-   }
-   nextCreditCheckAt = Date.now() + parseInt(get('x-post-rate-limit-reset') || '0') * 1000;
-   nextCreditCheckAt = Math.min(nextCreditCheckAt, parseInt(get('UserReset') || '0') * 1000);
-   console.log('now', uploadsAllowed, nextCreditCheckAt);
+	let get = (key: string) => responseHeadersOrCreditsApiData.get(key) || responseHeadersOrCreditsApiData.get('x-ratelimit-' + key.toLowerCase());
+	uploadsAllowed = Math.floor(parseInt(get('ClientRemaining') || '0') / 10);
+	uploadsAllowed = Math.min(uploadsAllowed, Math.floor(parseInt(get('UserRemaining') || '0') / 10));
+	let postsRemaining = get('x-post-rate-limit-remaining');
+	if (typeof postsRemaining === 'string') {
+		uploadsAllowed = Math.min(uploadsAllowed, parseInt(postsRemaining));
+	}
+	nextCreditCheckAt = Date.now() + parseInt(get('x-post-rate-limit-reset') || '0') * 1000;
+	nextCreditCheckAt = Math.min(nextCreditCheckAt, parseInt(get('UserReset') || '0') * 1000);
+	console.log('now', uploadsAllowed, nextCreditCheckAt);
 }
 
 let uploadQueue: [NextApiRequest,NextApiResponse][] = [];
 export default function upload(req: NextApiRequest, res: NextApiResponse) {
-   return new Promise<void>(resolve => {
-      res.on('close', resolve);
-      uploadQueue.push([req, res]);
-      startUploadFiberIfNotRunning();
-   });
+	return new Promise<void>(resolve => {
+		res.on('close', resolve);
+		uploadQueue.push([req, res]);
+		startUploadFiberIfNotRunning();
+	});
 }
 
 let uploadFiberRunning = false;
 async function startUploadFiberIfNotRunning() {
-   if (uploadFiberRunning) {
-      return;
-   }
-   try {
-      uploadFiberRunning = true;
-      while (uploadQueue.length > 0) {
-         let batch = uploadQueue;
-         uploadQueue = [];
-         for (let [req, res] of batch) {
-            try {
-               if (uploadsAllowed <= 0 && Date.now() >= nextCreditCheckAt) {
-                  console.log('needs credit check');
-                  try {
-                     let creditCheck = await fetch('https://api.imgur.com/3/credits', {
-                        headers: {
-                           authorization: 'Client-ID ' + clientId
-                        }
-                     });
-                     if (creditCheck.status !== 200) {
-                        throw new Error('https://api.imgur.com/3/credits returned ' + creditCheck.status);
-                     }
-                     let { data } = await creditCheck.json();
-                     updateRateLimitInfo(new Map(Object.entries(data as {[key:string]:string})));
-                  } catch (e) {
-                     console.error(e);
-                     nextCreditCheckAt = Date.now() + 15 * 60 * 1000;
-                  }
-               }
-               if (uploadsAllowed <= 0) {
-                  console.log('reached upload limit');
-                  res.status(503).send({ retryIn: Math.floor((nextCreditCheckAt - Date.now())/1000 + Math.random() * 60 ) });
-               } else {
-                  uploadsAllowed--;
-                  let uploadHeaders = await handleUpload(req, res);
-                  if (uploadHeaders) {
-                     updateRateLimitInfo(uploadHeaders);
-                  }
-               }
-            } catch (e) {
-               console.error('upload failed', e);
-               res.status(500).send('internal error');
-            }
-         }
-      }
-      uploadFiberRunning = false;
-   } catch (e) {
-      console.error('uncaught error in upload fiber', e);
-      process.exit(1);
-   }
+	if (uploadFiberRunning) {
+		return;
+	}
+	try {
+		uploadFiberRunning = true;
+		while (uploadQueue.length > 0) {
+			let batch = uploadQueue;
+			uploadQueue = [];
+			for (let [req, res] of batch) {
+				try {
+					if (uploadsAllowed <= 0 && Date.now() >= nextCreditCheckAt) {
+						console.log('needs credit check');
+						try {
+							let creditCheck = await fetch('https://api.imgur.com/3/credits', {
+								headers: {
+									authorization: 'Client-ID ' + clientId
+								}
+							});
+							if (creditCheck.status !== 200) {
+								throw new Error('https://api.imgur.com/3/credits returned ' + creditCheck.status);
+							}
+							let { data } = await creditCheck.json();
+							updateRateLimitInfo(new Map(Object.entries(data as {[key:string]:string})));
+						} catch (e) {
+							console.error(e);
+							nextCreditCheckAt = Date.now() + 15 * 60 * 1000;
+						}
+					}
+					if (uploadsAllowed <= 0) {
+						console.log('reached upload limit');
+						res.status(503).send({ retryIn: Math.floor((nextCreditCheckAt - Date.now())/1000 + Math.random() * 60 ) });
+					} else {
+						uploadsAllowed--;
+						let uploadHeaders = await handleUpload(req, res);
+						if (uploadHeaders) {
+							updateRateLimitInfo(uploadHeaders);
+						}
+					}
+				} catch (e) {
+					console.error('upload failed', e);
+					res.status(500).send('internal error');
+				}
+			}
+		}
+		uploadFiberRunning = false;
+	} catch (e) {
+		console.error('uncaught error in upload fiber', e);
+		process.exit(1);
+	}
 }
 
 async function handleUpload(req: NextApiRequest, res: NextApiResponse): Promise<Headers|undefined> {
-   try {
-      if (!clientId) {
-         console.error('upload failed, no IMGUR_CLIENT_ID environment variable');
-         res.status(500).send('no client id');
-         return;
-      }
-      let uploadResult = await fetch('https://api.imgur.com/3/image', {
-         method: 'POST',
-         headers: {
-            "authorization": "Client-ID " + clientId,
-            "content-type": req.headers["content-type"]!
-         },
-         body: await getRawBody(req)
-      });
-      if (uploadResult.status !== 200) {
-         console.error('upload failed with ' + uploadResult.status);
-         res.status(uploadResult.status).send('http error from imgur');
-         let body = await uploadResult.text();
-         console.error('from imgur: ' + body);
-         return uploadResult.headers;
-      }
-      let responseText = await uploadResult.text();
-      res.status(200);
-      res.setHeader('content-type', uploadResult.headers.get('content-type')!);
-      res.send(responseText);
-      return uploadResult.headers;
-   } catch (e) {
-      console.error('exception in upload', e);
-      res.status(500).send('caught exception');
-   }
+	try {
+		if (!clientId) {
+			console.error('upload failed, no IMGUR_CLIENT_ID environment variable');
+			res.status(500).send('no client id');
+			return;
+		}
+		let uploadResult = await fetch('https://api.imgur.com/3/image', {
+			method: 'POST',
+			headers: {
+				"authorization": "Client-ID " + clientId,
+				"content-type": req.headers["content-type"]!
+			},
+			body: await getRawBody(req)
+		});
+		if (uploadResult.status !== 200) {
+			console.error('upload failed with ' + uploadResult.status);
+			res.status(uploadResult.status).send('http error from imgur');
+			let body = await uploadResult.text();
+			console.error('from imgur: ' + body);
+			return uploadResult.headers;
+		}
+		let responseText = await uploadResult.text();
+		res.status(200);
+		res.setHeader('content-type', uploadResult.headers.get('content-type')!);
+		res.send(responseText);
+		return uploadResult.headers;
+	} catch (e) {
+		console.error('exception in upload', e);
+		res.status(500).send('caught exception');
+	}
 }
--- pages/upload-dialog.tsx
+++ pages/upload-dialog.tsx
@@ -3,182 +3,182 @@ import { PeerOffer, ModalService } from "poppyio";
 import { FormEvent, useCallback, useEffect, useState } from "react";
 
 export default function UploadDialog() {
-   let [viewLink, setViewLink] = useState<string>();
-   let [deleteLink, setDeleteLink] = useState<string>();
+	let [viewLink, setViewLink] = useState<string>();
+	let [deleteLink, setDeleteLink] = useState<string>();
 
-   let setLinks = useCallback((viewLink: string, deleteLink: string) => {
-      setViewLink(viewLink);
-      setDeleteLink(deleteLink);
-   }, []);
+	let setLinks = useCallback((viewLink: string, deleteLink: string) => {
+		setViewLink(viewLink);
+		setDeleteLink(deleteLink);
+	}, []);
 
-   return <>
-      <Head>
-         <title>Poppy.io i3r</title>
-      </Head>
-      <div id="body">
-         <h1><strong>Poppy.io i3r</strong> anonymous imgur upload</h1>
-         {
-            (viewLink || deleteLink)
-            ? <UploadResult viewLink={viewLink} deleteLink={deleteLink}/>
-            : <UploadForm setLinks={setLinks}/>
-         }
-      </div>
-   </>
+	return <>
+		<Head>
+			<title>Poppy.io i3r</title>
+		</Head>
+		<div id="body">
+			<h1><strong>Poppy.io i3r</strong> anonymous imgur upload</h1>
+			{
+				(viewLink || deleteLink)
+				? <UploadResult viewLink={viewLink} deleteLink={deleteLink}/>
+				: <UploadForm setLinks={setLinks}/>
+			}
+		</div>
+	</>
 }
 
 function UploadForm(props: { setLinks: (viewLink: string, deleteLink: string) => void }) {
-   let { setLinks } = props;
-   let [message, setMessage] = useState('Waiting for request...');
-   let [imageBlob, setImageBlob] = useState<Blob>();
-   let [hasValidImage, setHasValidImage] = useState(false);
+	let { setLinks } = props;
+	let [message, setMessage] = useState('Waiting for request...');
+	let [imageBlob, setImageBlob] = useState<Blob>();
+	let [hasValidImage, setHasValidImage] = useState(false);
 
-   let [title, setTitle] = useState('');
-   let [description, setDescription] = useState('');
-   let [completeRequest, setCompleteRequest] = useState<(viewLink: string, deleteLink: string)=>void>();
+	let [title, setTitle] = useState('');
+	let [description, setDescription] = useState('');
+	let [completeRequest, setCompleteRequest] = useState<(viewLink: string, deleteLink: string)=>void>();
 
-   let [working, setWorking] = useState(false);
+	let [working, setWorking] = useState(false);
 
-   useEffect(() => {
-      let resolveExchange: (() => void) | undefined;
-      ModalService.getRequest().then(async request => {
-         if (!request) {
-            setMessage('No request detected; this page should be opened from a poppy');
-            return;
-         }
-         let result = await request.open({
-            accepting: 'content-blob',
-            using: (offer: PeerOffer) => new Promise<void>(resolve => {
-               let envelope = offer.data[0];
-               if (envelope && envelope.blob instanceof Blob) {
-                  setImageBlob(envelope.blob);
-                  setCompleteRequest(() => async (viewLink: string, deleteLink: string) => {
-                     await offer.postResult({
-                        links: [
-                           { title: 'View Image', href: viewLink },
-                           { title: 'Delete Image', href: deleteLink }
-                        ]
-                     });
-                     setLinks(viewLink, deleteLink);
-                     resolve();
-                  });
-                  if (typeof envelope.title === 'string') {
-                     setTitle(envelope.title);
-                  }
-                  if (typeof envelope.description === 'string') {
-                     setDescription(envelope.description);
-                  }
-               } else {
-                  setMessage('')
-               }
-            })
-         })
-         if (!result.matched) {
-            setMessage('Client is not compatible; expecting a content-blob offer');
-         }
-      });
-      return () => { if (resolveExchange) resolveExchange() }
-   }, [setLinks]);
-   useEffect(() => {
-      if (imageBlob) {
-         setMessage('Generating thumbnail...');
-         setHasValidImage(false);
-      }
-   }, [imageBlob]);
-   let thumbnailLoaded = () => {
-      setMessage('Ready for upload');
-      setHasValidImage(true);
-   };
-   let thumbnailFailed = () => {
-      setMessage('Image failed to load; probably not supported');
-   };
+	useEffect(() => {
+		let resolveExchange: (() => void) | undefined;
+		ModalService.getRequest().then(async request => {
+			if (!request) {
+				setMessage('No request detected; this page should be opened from a poppy');
+				return;
+			}
+			let result = await request.open({
+				accepting: 'content-blob',
+				using: (offer: PeerOffer) => new Promise<void>(resolve => {
+					let envelope = offer.data[0];
+					if (envelope && envelope.blob instanceof Blob) {
+						setImageBlob(envelope.blob);
+						setCompleteRequest(() => async (viewLink: string, deleteLink: string) => {
+							await offer.postResult({
+								links: [
+									{ title: 'View Image', href: viewLink },
+									{ title: 'Delete Image', href: deleteLink }
+								]
+							});
+							setLinks(viewLink, deleteLink);
+							resolve();
+						});
+						if (typeof envelope.title === 'string') {
+							setTitle(envelope.title);
+						}
+						if (typeof envelope.description === 'string') {
+							setDescription(envelope.description);
+						}
+					} else {
+						setMessage('')
+					}
+				})
+			})
+			if (!result.matched) {
+				setMessage('Client is not compatible; expecting a content-blob offer');
+			}
+		});
+		return () => { if (resolveExchange) resolveExchange() }
+	}, [setLinks]);
+	useEffect(() => {
+		if (imageBlob) {
+			setMessage('Generating thumbnail...');
+			setHasValidImage(false);
+		}
+	}, [imageBlob]);
+	let thumbnailLoaded = () => {
+		setMessage('Ready for upload');
+		setHasValidImage(true);
+	};
+	let thumbnailFailed = () => {
+		setMessage('Image failed to load; probably not supported');
+	};
 
-   let performUpload = async (ev: FormEvent) => {
-      ev.preventDefault();
-      setWorking(true);
-      let completed = false;
-      try {
-         let body = new FormData();
-         body.append('image', imageBlob!);
-         if (title) body.append('title', title);
-         if (description) body.append('description', description);
-         let res = await fetch('/api/imgur/upload', {
-            method: 'POST',
-            body
-         });
-         if (res.status === 503) {
-            try {
-               let json = await res.json();
-               if (json && typeof json.retryIn === 'number') {
-                  setMessage('Rate limited... maybe try again in ' + json.retryIn + ' seconds');
-                  return;
-               }
-            } catch (e) {
-               // ignore
-            }
-            setMessage('Server too busy right now');
-            return;
-         } else if (res.status !== 200) {
-            setMessage('Error - HTTP ' + res.status + ' ' + res.statusText);
-            return;
-         }
-         let { data } = await res.json();
-         completed = true;
-         completeRequest!(data.link, 'https://imgur.com/delete/' + data.deletehash);
-      } catch (e: any) {
-         console.error(e);
-         setMessage('There was an error');
-      } finally {
-         setWorking(completed);
-      }
-   };
+	let performUpload = async (ev: FormEvent) => {
+		ev.preventDefault();
+		setWorking(true);
+		let completed = false;
+		try {
+			let body = new FormData();
+			body.append('image', imageBlob!);
+			if (title) body.append('title', title);
+			if (description) body.append('description', description);
+			let res = await fetch('/api/imgur/upload', {
+				method: 'POST',
+				body
+			});
+			if (res.status === 503) {
+				try {
+					let json = await res.json();
+					if (json && typeof json.retryIn === 'number') {
+						setMessage('Rate limited... maybe try again in ' + json.retryIn + ' seconds');
+						return;
+					}
+				} catch (e) {
+					// ignore
+				}
+				setMessage('Server too busy right now');
+				return;
+			} else if (res.status !== 200) {
+				setMessage('Error - HTTP ' + res.status + ' ' + res.statusText);
+				return;
+			}
+			let { data } = await res.json();
+			completed = true;
+			completeRequest!(data.link, 'https://imgur.com/delete/' + data.deletehash);
+		} catch (e: any) {
+			console.error(e);
+			setMessage('There was an error');
+		} finally {
+			setWorking(completed);
+		}
+	};
 
-   let disabled = working || !hasValidImage;
-   let disabledClass = disabled ? 'disabled' : undefined;
+	let disabled = working || !hasValidImage;
+	let disabledClass = disabled ? 'disabled' : undefined;
 
-   return <div id="uploader">
-      <div id="message">{message}</div>
-      <div id="image"><BlobThumbnail blob={imageBlob} onLoad={thumbnailLoaded} onError={thumbnailFailed}/></div>
-      <form id="mform" onSubmit={performUpload}>
-         <fieldset className={disabledClass}>
-            <label htmlFor="title">Title</label>
-            <input id="title" disabled={disabled} type="text" value={title} onChange={e => setTitle(e.target.value)}/>
-         </fieldset>
-         <fieldset className={disabledClass}>
-            <label htmlFor="description">Description</label>
-            <textarea id="description" disabled={disabled} value={description} onChange={e => setDescription(e.target.value)}/>
-         </fieldset>
-         <div id="buttons">
-            <button className={disabledClass} disabled={disabled} type="submit">upload to imgur</button>
-            {" "}
-            <button type="button" className="close" onClick={() => ModalService.close()}>cancel - close and return to client</button>
-         </div>
-      </form>
-   </div>;
+	return <div id="uploader">
+		<div id="message">{message}</div>
+		<div id="image"><BlobThumbnail blob={imageBlob} onLoad={thumbnailLoaded} onError={thumbnailFailed}/></div>
+		<form id="mform" onSubmit={performUpload}>
+			<fieldset className={disabledClass}>
+				<label htmlFor="title">Title</label>
+				<input id="title" disabled={disabled} type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+			</fieldset>
+			<fieldset className={disabledClass}>
+				<label htmlFor="description">Description</label>
+				<textarea id="description" disabled={disabled} value={description} onChange={e => setDescription(e.target.value)}/>
+			</fieldset>
+			<div id="buttons">
+				<button className={disabledClass} disabled={disabled} type="submit">upload to imgur</button>
+				{" "}
+				<button type="button" className="close" onClick={() => ModalService.close()}>cancel - close and return to client</button>
+			</div>
+		</form>
+	</div>;
 }
 
 function BlobThumbnail(props: { blob: Blob|undefined, onLoad?: () =>void, onError?: () => void } ) {
-   let { blob, onLoad, onError } = props;
-   let [url, setUrl] = useState<string|undefined>();
-   useEffect(() => {
-      if (blob) {
-         let url = URL.createObjectURL(blob);
-         setUrl(url);
-         return () => URL.revokeObjectURL(url);
-      }
-   }, [blob]);
-   if (url) {
-      return <img alt="Offered Image" src={url} style={{maxWidth: '150px', maxHeight: '150px'}} onLoad={onLoad} onError={onError}/>
-   } else {
-      return null;
-   }
+	let { blob, onLoad, onError } = props;
+	let [url, setUrl] = useState<string|undefined>();
+	useEffect(() => {
+		if (blob) {
+			let url = URL.createObjectURL(blob);
+			setUrl(url);
+			return () => URL.revokeObjectURL(url);
+		}
+	}, [blob]);
+	if (url) {
+		return <img alt="Offered Image" src={url} style={{maxWidth: '150px', maxHeight: '150px'}} onLoad={onLoad} onError={onError}/>
+	} else {
+		return null;
+	}
 }
 
 function UploadResult(props: { viewLink: string|undefined, deleteLink: string|undefined }) {
-   return <div id='afterUpload'>
-      <p>Your image was uploaded successfully.</p>
-      <p>These links were sent back to the client, but in case you don&apos;t see them there:</p>
-      <p>You can view it on imgur at <code>{props.viewLink}</code></p>                        
-      <p>Save this link if you want to delete the image: <code>{props.deleteLink}</code></p>
-      <button className='close' onClick={() => ModalService.close()}>done - close and return to client</button>
-   </div>;
+	return <div id='afterUpload'>
+		<p>Your image was uploaded successfully.</p>
+		<p>These links were sent back to the client, but in case you don&apos;t see them there:</p>
+		<p>You can view it on imgur at <code>{props.viewLink}</code></p>                        
+		<p>Save this link if you want to delete the image: <code>{props.deleteLink}</code></p>
+		<button className='close' onClick={() => ModalService.close()}>done - close and return to client</button>
+	</div>;
 }
